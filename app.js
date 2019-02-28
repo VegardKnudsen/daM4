e6cfb4f7eec103b72d82a8e1e48d8e22c19d0fc8
Vegard <v.steinsto@gmail.com>
2019-02-28 08:55:45 +0100

    Comment for all the functions

--- app.js
+++ app.js
@@ -15,6 +15,19 @@ app.get('/', (req, res) => {
     res.send('Default route');
 });
 
+app.post('/post', (req, res) => {
+    res.send("Post request");
+})
+
+/*
+*
+* AUTHORS
+*
+*/
+
+/*
+* GET
+*/
 app.get('/authors/:authorid', (req, res) => {
     const authorLookup = req.params.authorid;
 
@@ -35,6 +48,9 @@ app.get('/authors/:authorid', (req, res) => {
     );
 });
 
+/*
+* GET
+*/
 app.get('/authors', (req, res) => {
     //const authorsLookup = req.params.authors;
 
@@ -53,47 +69,9 @@ app.get('/authors', (req, res) => {
     );
 });
 
-app.get('/books/:bookid', (req, res) => {
-    const bookLookup = req.params.bookid;
-
-    db.all(
-        'SELECT * FROM book WHERE bookID=$bookID',
-        {
-            $bookID: bookLookup
-        },
-        (err, rows) => {
-            //console.log(rows);
-            if(rows.length > 0){
-                var xmlResponse = js2xmlparser.parse("book", rows);
-                res.send(xmlResponse);
-            } 
-            else {
-                res.send("Couldn't find anything...")
-            }
-        }
-    );
-});
-
-app.get('/books', (req, res) => {
-    db.all(
-        'SELECT * FROM book',
-        (err, rows) => {
-            console.log(rows);
-            if(rows.length > 0){
-                //res.send(rows);
-                res.send(js2xmlparser.parse("books", rows));
-            } 
-            else {
-                res.send("Couldn't find anything...");
-            }
-        }
-    );
-});
-
-app.post('/post', (req, res) => {
-    res.send("Post request");
-})
-
+/*
+* POST
+*/
 app.post('/authors', (req, res) => {
     var SQL = `INSERT INTO author(authorID, firstname, lastname, nationality) VALUES(?,?,?,?);`;
     var data = {
@@ -118,54 +96,9 @@ app.post('/authors', (req, res) => {
     res.send("ok");
 });
 
-app.post('/books', (req, res) => {
-    var SQL = `INSERT INTO book(bookID, booktitle, authorID) VALUES(?,?,?);`;
-    var data = {
-        bookID: req.body.bookID,
-        booktitle: req.body.booktitle,
-        authorID: req.body.authorID
-    }
-    var params = [data.bookID, data.booktitle, data.authorID];
-
-    console.log(data);
-
-    //db.run(sql, params, function(err))
-    db.run(SQL, params, (err) => {
-        if(err){
-            console.log("Something went wrong!");
-            res.send("Something went wrong!")
-        }
-        else{
-            console.log("Data added!");
-            res.send("Ok!")
-        }
-    });
-});
-
-app.put('/books', (req, res) => {
-    var SQL = `UPDATE book SET booktitle = ?, authorID = ? WHERE bookID = ?`;
-    var data = {
-        booktitle: req.body.booktitle, 
-        authorID: req.body.authorID,
-        bookID: req.body.bookID
-    }
-    var params = [data.booktitle, data.authorID, data.bookID];
-
-    console.log(data);
-
-    //db.run(sql, params, function(err))
-    db.run(SQL, params, (err) => {
-        if(err){
-            console.log("Something went wrong!");
-            res.send("Something went wrong!")
-        }
-        else{
-            console.log("Data added!");
-            res.send("Ok!")
-        }
-    });
-});
-
+/*
+* PUT
+*/
 app.put('/authors', (req, res) => {
     var SQL = `UPDATE author SET firstname = ?, lastname = ?, nationality = ? WHERE authorID = ?`;
     var data = {
@@ -191,38 +124,9 @@ app.put('/authors', (req, res) => {
     });
 });
 
-app.delete('/books', (req, res) => {
-    var data = {
-        bookID: req.body.bookID
-    }
-    if (req.body.bookID) {
-    	var SQL = `DELETE FROM book WHERE bookID = ?`;
-    	var params = [data.bookID];
-
-    	db.run(SQL, params, (err) => {
-        	if(err){
-            	console.log("Something went wrong!");
-            	res.send("Something went wrong!")
-        	}else{
-            	console.log("Data deleted!");
-            	res.send("Data deleted!")
-        	}
-    	});
-    } else {
-    	var SQL = `DELETE FROM book`;
-
-    	db.run(SQL, (err) => {
-        	if(err){
-            	console.log("Something went wrong!");
-            	res.send("Something went wrong!")
-        	}else{
-            	console.log("Data deleted!");
-            	res.send("Data deleted!")
-        	}
-    	});
-    }
-});
-
+/*
+* DELETE
+*/
 app.delete('/authors', (req, res) => {
     var data = {
         authorID: req.body.authorID
@@ -255,10 +159,148 @@ app.delete('/authors', (req, res) => {
         	}
     	});
     }
+    console.log(data);    
+});
+
+
+/*
+*
+* BOOKS
+*
+*/
+
+/*
+* GET
+*/
+app.get('/books/:bookid', (req, res) => {
+    const bookLookup = req.params.bookid;
+
+    db.all(
+        'SELECT * FROM book WHERE bookID=$bookID',
+        {
+            $bookID: bookLookup
+        },
+        (err, rows) => {
+            //console.log(rows);
+            if(rows.length > 0){
+                var xmlResponse = js2xmlparser.parse("book", rows);
+                res.send(xmlResponse);
+            } 
+            else {
+                res.send("Couldn't find anything...")
+            }
+        }
+    );
+});
+
+/*
+* GET
+*/
+app.get('/books', (req, res) => {
+    db.all(
+        'SELECT * FROM book',
+        (err, rows) => {
+            console.log(rows);
+            if(rows.length > 0){
+                //res.send(rows);
+                res.send(js2xmlparser.parse("books", rows));
+            } 
+            else {
+                res.send("Couldn't find anything...");
+            }
+        }
+    );
+});
+
+
+/*
+* POST
+*/
+app.post('/books', (req, res) => {
+    var SQL = `INSERT INTO book(bookID, booktitle, authorID) VALUES(?,?,?);`;
+    var data = {
+        bookID: req.body.bookID,
+        booktitle: req.body.booktitle,
+        authorID: req.body.authorID
+    }
+    var params = [data.bookID, data.booktitle, data.authorID];
+
     console.log(data);
 
     //db.run(sql, params, function(err))
-    
+    db.run(SQL, params, (err) => {
+        if(err){
+            console.log("Something went wrong!");
+            res.send("Something went wrong!")
+        }
+        else{
+            console.log("Data added!");
+            res.send("Ok!")
+        }
+    });
 });
+
+/*
+* PUT
+*/
+app.put('/books', (req, res) => {
+    var SQL = `UPDATE book SET booktitle = ?, authorID = ? WHERE bookID = ?`;
+    var data = {
+        booktitle: req.body.booktitle, 
+        authorID: req.body.authorID,
+        bookID: req.body.bookID
+    }
+    var params = [data.booktitle, data.authorID, data.bookID];
+
+    console.log(data);
+
+    //db.run(sql, params, function(err))
+    db.run(SQL, params, (err) => {
+        if(err){
+            console.log("Something went wrong!");
+            res.send("Something went wrong!")
+        }
+        else{
+            console.log("Data added!");
+            res.send("Ok!")
+        }
+    });
+});
+
+/*
+* DELETE
+*/
+app.delete('/books', (req, res) => {
+    var data = {
+        bookID: req.body.bookID
+    }
+    if (req.body.bookID) {
+    	var SQL = `DELETE FROM book WHERE bookID = ?`;
+    	var params = [data.bookID];
+
+    	db.run(SQL, params, (err) => {
+        	if(err){
+            	console.log("Something went wrong!");
+            	res.send("Something went wrong!")
+        	}else{
+            	console.log("Data deleted!");
+            	res.send("Data deleted!")
+        	}
+    	});
+    } else {
+    	var SQL = `DELETE FROM book`;
+
+    	db.run(SQL, (err) => {
+        	if(err){
+            	console.log("Something went wrong!");
+            	res.send("Something went wrong!")
+        	}else{
+            	console.log("Data deleted!");
+            	res.send("Data deleted!")
+        	}
+    	});
+    }
+});
+
 
 app.listen(port, () => console.log('Listening on port ' + port));
